dc98de0a801b61558838c2b99e1f39fbee96655d
ezequielmartinb10@gmail.com <ezequielmartinb10@gmail.com>
2025-05-21 15:04:13 -0300

    Cambios en el juego del ahorcado. Se reducen la cantidad de errores y se dibuja primero la horca y luego a la persona

---
+++ public/chunk-LTPO3EAX.js
@@ -0,0 +1,1 @@
+import{a}from"./chunk-P25V4NQ2.js";import"./chunk-4JRDNEFN.js";import"./chunk-VJLT3EIH.js";import"./chunk-XNZRBX6S.js";import"./chunk-FK42CRUA.js";export{a as AhorcadoComponent};
--- src/app/componentes/ahorcado/ahorcado.component.ts
+++ src/app/componentes/ahorcado/ahorcado.component.ts
@@ -22,24 +22,29 @@ export class AhorcadoComponent implements AfterViewInit
   palabraOculta: string = "";
   letras: string[] = [];
   errores = 0;
-  erroresMaximos = 11;  
+  erroresMaximos = 7;  
   juegoActivo = true;
   cargandoPalabra:boolean = true;
+  mensajeFinal:string = "";
 
   constructor(private palabrasService: PalabrasSecretasService) {}
 
-  ngOnInit() {
+  ngOnInit() 
+  {
     this.cargandoPalabra = true; 
-    this.palabrasService.getPalabrasSecretas().subscribe({
-      next: (data) => {
-        
-        this.palabrasSecretas = data
-          .filter(palabra => !palabra.includes(" ") && !palabra.includes("-") && !palabra.includes(".") && !palabra.includes("!"))
-          .map(palabra => palabra.normalize("NFD").replace(/[\u0300-\u036f]/g, "").toUpperCase());
+    this.palabrasService.getPalabrasSecretas().subscribe(
+    {
+      next: (data) => 
+      {        
+        this.palabrasSecretas = data.filter(palabra => !palabra.includes(" ") && !palabra.includes("-") && !palabra.includes(".") && !palabra.includes("!"))
+        .map(palabra => palabra.normalize("NFD").replace(/[\u0300-\u036f]/g, "").toUpperCase());
   
-        if (this.palabrasSecretas.length > 0) {
+        if (this.palabrasSecretas.length > 0) 
+        {
           this.iniciarJuego();
-        } else {
+        } 
+        else 
+        {
           console.error("Error: No se recibieron palabras válidas.");
         }
   
@@ -63,26 +68,16 @@ export class AhorcadoComponent implements AfterViewInit
     {
       console.error("No hay palabras disponibles para jugar.");
       return;
-    }  
-    
-    const palabrasFiltradas = this.palabrasSecretas.filter(palabra => !palabra.includes(" ") && !palabra.includes("-") && !palabra.includes(".") && !palabra.includes("!"))
-    .map(palabra => palabra.normalize("NFD").replace(/[\u0300-\u036f]/g, ""));
-  
-    if (palabrasFiltradas.length === 0) 
-    {
-      console.error("No quedan palabras válidas después del filtrado.");
-      return;
-    }
-  
-    this.palabraIngresada = palabrasFiltradas[Math.trunc(Math.random() * palabrasFiltradas.length)];
-  
+    }   
+     
+    this.palabraIngresada = this.palabrasSecretas[Math.trunc(Math.random() * this.palabrasSecretas.length)];  
     this.palabraOculta = '_ '.repeat(this.palabraIngresada.length).trim();
     this.letras = [];
     this.errores = 0;
     this.juegoActivo = true;
   
-    console.log("Palabra seleccionada sin acentos y sin espacios:", this.palabraIngresada);
-  
+    console.log("Palabra seleccionada:", this.palabraIngresada);
+    this.mensajeFinal = "";  
     this.limpiarAhorcado();
   }
   mostrarLetraElegida(letter: string) 
@@ -100,7 +95,7 @@ export class AhorcadoComponent implements AfterViewInit
       this.actualizarPalabraOculta(letter);
     } 
     else 
-    {
+    {      
       this.mostrarError();
     }
 
@@ -119,9 +114,10 @@ export class AhorcadoComponent implements AfterViewInit
   }
 
   mostrarError() 
-  {
+  {    
     if (this.errores < this.erroresMaximos) 
     {
+      this.dibujarHorca();
       this.errores++;
       this.dibujarAhorcado();
     }
@@ -129,17 +125,17 @@ export class AhorcadoComponent implements AfterViewInit
 
   informarResultadoDelJuego() 
   {
-    if (!this.palabraOculta.includes('_')) 
+    if (!this.palabraOculta.includes('_'))
     {
       this.juegoActivo = false;
-      setTimeout(() => alert(`Ganaste! La palabra es: ${this.palabraIngresada}`), 300);
+      this.mensajeFinal = `Ganaste! La palabra es: ${this.palabraIngresada}`;
     } 
     else if (this.errores >= this.erroresMaximos) 
     {
       this.juegoActivo = false;
-      setTimeout(() => alert(`Perdiste! La palabra era: ${this.palabraIngresada}`), 300);
+      this.mensajeFinal = `Perdiste! La palabra era: ${this.palabraIngresada}`;
     }
-  }
+  } 
 
   limpiarAhorcado()
   {
@@ -148,11 +144,8 @@ export class AhorcadoComponent implements AfterViewInit
 
   dibujarAhorcado() 
   {
-    const dibujo = [
-      () => this.dibujarLineas(70, 220, 130, 220),
-      () => this.dibujarLineas(100, 50, 100, 220),
-      () => this.dibujarLineas(100, 50, 150, 50),
-      () => this.dibujarLineas(150, 50, 150, 60),
+    const dibujoDePersona = 
+    [            
       () => this.dibujarCabeza(150, 80, 20),
       () => this.dibujarLineas(150, 100, 150, 160), 
       () => this.dibujarLineas(150, 110, 130, 140), 
@@ -162,17 +155,32 @@ export class AhorcadoComponent implements AfterViewInit
       () => this.dibujarLineas(120, 100, 180, 100)
     ];
 
-    if (this.errores <= dibujo.length) 
-      {
-      dibujo[this.errores - 1]();
+    if (this.errores <= dibujoDePersona.length) 
+    {
+      dibujoDePersona[this.errores - 1]();
     }
   }  
+  dibujarHorca()
+  {
+    const dibujoDeHorca = 
+    [
+      () => this.dibujarLineas(70, 220, 130, 220),
+      () => this.dibujarLineas(100, 50, 100, 220),
+      () => this.dibujarLineas(100, 50, 150, 50),
+      () => this.dibujarLineas(150, 50, 150, 60),
+      () => this.dibujarCabeza(150, 80, 20)
+    ]
+    for(const index in dibujoDeHorca)
+    {
+      dibujoDeHorca[index]();
+    }
+  }
 
   dibujarCabeza(x: number, y: number, radius: number) 
   {
     this.ctx.beginPath();
     this.ctx.arc(x, y, radius, 0, Math.PI * 2);
-    this.ctx.strokeStyle = '#FFFFFF'; // Color blanco
+    this.ctx.strokeStyle = '#FFFFFF';
     this.ctx.lineWidth = 3;
     this.ctx.stroke();
   }
